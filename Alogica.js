3eecb38ab3dd8e2ab07e8f49ae64549103d51377
Felipe Leandro <felipellima286@gmail.com>
2022-06-06 22:19:49 -0300

    first commit

---
+++ Alogica.js
@@ -0,0 +1,32 @@
+function calculoimc(){
+    let peso = parseFloat(document.getElementById("peso").value.trim())
+    let altura = parseFloat(document.getElementById("altura").value.trim())
+    let imc = 0;
+    let resultado="";
+ 
+    imc = peso / (altura * altura); 
+ 
+    if (imc <18.5){
+        resultado = "abaixo do peso."
+    }
+    else if (imc <=25){
+        resultado = "peso normal."
+    }
+    else if (imc <=30){
+        resultado = "sobrepeso"
+    }
+    else if (imc <=35){
+        resultado = "obesidade"
+    }
+    else if (imc >=35 && imc <40){
+        resultado ="obesidade 2"
+    }
+    else if (imc >=40){
+        resultado ="obesidade 3"
+    }
+    console.log(resultado)
+    console.log(imc)
+ 
+    document.write(nome + " possui o imc " + imc + " e está " + resultado);
+    alert(nome + " possui o imc " + imc + " e está " + resultado);
+}
